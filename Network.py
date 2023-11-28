481a27a7cc0952ef666e2755a0b27b60544b954e
CTC <2801320287@qq.com>
2023-11-28 13:58:07 +0800

    Updated on 2023.11.28

--- Network.py
+++ Network.py
@@ -2,7 +2,7 @@
 Author: CTC 2801320287@qq.com
 Date: 2023-11-25 14:39:45
 LastEditors: CTC 2801320287@qq.com
-LastEditTime: 2023-11-26 17:26:02
+LastEditTime: 2023-11-27 20:04:08
 Description: 
 
 Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
@@ -23,25 +23,29 @@ def GET_DEVICE(device=0):
             print(f"No such cuda device: {device}")
     return "cpu"
 
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
 
 class MyAutoencoder(nn.Module):
     # 3 Layer MLP Encoder & Decoder Attempt
-    def __init__(self, input_size, hidden_size_1, hidden_size_2, dropout_prob=0.5):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, dropout_prob=0.1):
         super(MyAutoencoder, self).__init__()
         self.encoder = nn.Sequential(
             nn.Linear(input_size, hidden_size_1),
             nn.ReLU(),
-            # nn.BatchNorm1d(hidden_size_1),
+            nn.BatchNorm1d(hidden_size_1),
             nn.Dropout(dropout_prob),
             nn.Linear(hidden_size_1, hidden_size_2),
             nn.ReLU(),
-            # nn.BatchNorm1d(hidden_size_2),
+            nn.BatchNorm1d(hidden_size_2),
             nn.Dropout(dropout_prob),
         )
         self.decoder = nn.Sequential(
             nn.Linear(hidden_size_2, hidden_size_1),
             nn.ReLU(),
-            # nn.BatchNorm1d(hidden_size_1),
+            nn.BatchNorm1d(hidden_size_1),
             nn.Dropout(dropout_prob),
             nn.Linear(hidden_size_1, input_size),
         )
